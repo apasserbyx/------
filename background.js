aff0b66432ebf4a3d75997823777545a72b3d181
apasserbyx <3310687180@qq.com>
2024-12-28 19:09:57 +0800

    Update background.js
    添加更多的菜单选项，相应地需要创建更多的 chrome.contextMenus.create 条目，并添加更多的 onClicked 监听器。
    添加对不同菜单选项的不同操作逻辑，例如除了隐藏元素，还可以添加显示元素、修改元素样式等操作，会导致 onClicked 回调函数中的逻辑更加复杂。

--- background.js
+++ background.js
@@ -1,80 +1,26 @@
 // 定义常量
-const MENU_ITEM_ID = "hideElement";
+const MENU_ITEM_ID_HIDE = "hideElement";
+const MENU_ITEM_ID_SHOW = "showElement";
 const ACTION_HIDE_ELEMENT = "hideElement";
-const MENU_ITEM_ID_BLOCK_SAME_TYPE = "blockSameTypeAd";
-const ACTION_BLOCK_SAME_TYPE = "blockSameTypeAd";
-const MENU_ITEM_ID_VIEW_STATS = "viewAdBlockingStats";
-const ACTION_VIEW_STATS = "viewAdBlockingStats";
-<<<<<<< HEAD
-
-=======
->>>>>>> 08e31a299be7bf5dad747a0db3d002dd2d2fdedf
-// 当扩展安装时，上下文菜单
+const ACTION_SHOW_ELEMENT = "showElement";
+// 当扩展安装时，创建上下文菜单
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: MENU_ITEM_ID,
+    id: MENU_ITEM_ID_HIDE,
     title: "Hide this element",
     contexts: ["all"]
   });
+  chrome.contextMenus.create({
+    id: MENU_ITEM_ID_SHOW,
+    title: "Show this element",
+    contexts: ["all"]
+  });
 });
-
 // 当上下文菜单被点击时，发送消息给标签页
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === MENU_ITEM_ID) {
+  if (info.menuItemId === MENU_ITEM_ID_HIDE) {
     chrome.tabs.sendMessage(tab.id, { action: ACTION_HIDE_ELEMENT });
-  }
-});chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: MENU_ITEM_ID,
-    title: "Hide this element",
-    tooltip: "隐藏当前右键点击的广告元素",
-    contexts: ["all"]
-});
-  chrome.contextMenus.create({
-    id: MENU_ITEM_ID_BLOCK_SAME_TYPE,
-    title: "屏蔽此类广告",
-    tooltip: "屏蔽与当前广告元素具有相同特征的所有广告",
-    contexts: ["all"]
-});
-chrome.contextMenus.create({
-    id: MENU_ITEM_ID_VIEW_STATS,
-    title: "查看广告拦截统计",
-    tooltip: "查看插件拦截广告的相关统计信息",
-    contexts: ["all"]
-<<<<<<< HEAD
-
-=======
->>>>>>> 08e31a299be7bf5dad747a0db3d002dd2d2fdedf
-});//为每个菜单选项添加提示文字，使用户清楚每个选项的功能
-});
-
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "hideElement") {
-    chrome.tabs.sendMessage(tab.id, { action: "hideElement" });
+  } else if (info.menuItemId === MENU_ITEM_ID_SHOW) {
+    chrome.tabs.sendMessage(tab.id, { action: ACTION_SHOW_ELEMENT });
   }
 });
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-      id: MENU_ITEM_ID,
-      title: "Hide this element",
-      contexts: ["all"]
-  });
-  chrome.contextMenus.create({
-      id: MENU_ITEM_ID_BLOCK_SAME_TYPE,
-      title: "屏蔽此类广告",
-      contexts: ["all"]
-  });
-});
-
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === MENU_ITEM_ID) {
-      chrome.tabs.sendMessage(tab.id, { action: ACTION_HIDE_ELEMENT });
-  } else if (info.menuItemId === MENU_ITEM_ID_BLOCK_SAME_TYPE) {
-      chrome.tabs.sendMessage(tab.id, { action: ACTION_BLOCK_SAME_TYPE });
-  }
-});//为右键菜单添加一键屏蔽具有相同特征的所有广告
-<<<<<<< HEAD
-=======
-
->>>>>>> 08e31a299be7bf5dad747a0db3d002dd2d2fdedf
